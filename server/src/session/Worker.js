38ee0cfafe4d28a1b8c01378e3611b46549fa1b6
Jorge Prendes <jorge.prendes@gmail.com>
2022-07-19 10:53:38 +0100

    Do a health check when a worker connects

--- server/src/session/Worker.js
+++ server/src/session/Worker.js
@@ -1,5 +1,7 @@
 import { v4 as uuidv4 } from "uuid";
 import { IpcClient } from "cloudtitan-common/comm/Ipc.js";
+import Owner from "cloudtitan-common/events/Owner.js";
+import Api from "./Api.js";
 
 class Worker {
     #sock = null;
@@ -20,18 +22,42 @@ class Worker {
         return this.#running;
     }
 
+    async #runTask(task) {
+        try {
+            const channel = this.#sock.channel(uuidv4());
+            const ipc = new IpcClient(channel);
+            try {
+                await task(ipc.proxy());
+                return true;
+            } catch (err) {
+                console.error("Worker failed task");
+                console.error(err);
+                return false;
+            } finally {
+                await ipc.close();
+            }
+        } catch (err) {
+            console.error("Error with IPC channel.");
+            console.error(err);
+            return false;
+        }
+    }
+
     async #run() {
+        if (!await this.#runTask(Api.healthcheck)) {
+            // Health check failed.
+            // Wait a while, and then close connection.
+            // This is to avoid fast reconnects.
+            await Owner.timeout(300e3);
+            return;
+        }
+
         const tasks = this.#queue.tasks();
         this.#sock.on("close", () => tasks.abort());
 
         for await (const task of tasks) {
-            try {
-                const channel = this.#sock.channel(uuidv4());
-                const ipc = new IpcClient(channel);
-                await task(ipc.proxy());
-                await ipc.close();
-            } catch (err) {
-                console.error(err);
+            const success = await this.#runTask(task);
+            if (!success) {
                 this.#queue.unshift(task);
             }
         }
