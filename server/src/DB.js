b7ce4b356a8d485f5eddf7e06881857bf2162f69
Jorge Prendes <jorge.prendes@gmail.com>
2022-07-21 11:22:02 +0100

    Use packager in database

--- server/src/DB.js
+++ server/src/DB.js
@@ -1,7 +1,19 @@
 import { Level } from "level";
+import * as pkg from "cloudtitan-common/comm/Packager.js";
 import { DB_ROOT } from "./config.js";
 
-const db = new Level(DB_ROOT, { valueEncoding: "json" });
+function serialize(value) {
+    return Buffer.concat([Buffer.from("pkg1"), pkg.serialize(value)]);
+}
+
+function deserialize(buffer) {
+    if (buffer.slice(0, 4).toString() === "pkg1") {
+        return pkg.deserialize(buffer.slice(4));
+    }
+    return JSON.parse(buffer.toString());
+}
+
+const db = new Level(DB_ROOT, { valueEncoding: "buffer" });
 
 class DB {
     #db = null;
@@ -10,33 +22,40 @@ class DB {
         if (!name) {
             this.#db = db;
         } else {
-            this.#db = db.sublevel(name, { valueEncoding: "json" });
+            this.#db = db.sublevel(name, { valueEncoding: "buffer" });
         }
     }
 
     set(key, value) {
+        value = serialize(value);
         return this.#db.put(key, value);
     }
 
     async get(key) {
         const [value] = await this.#db.getMany([key]);
-        return value;
+        return deserialize(value);
     }
 
     delete(key) {
         this.#db.del(key);
     }
 
-    iterator() {
-        return this.#db.iterator();
+    async* iterator() {
+        for await (const [key, value] of this.#db.iterator()) {
+            yield [key, deserialize(value)];
+        }
     }
 
-    keys() {
-        return this.#db.keys();
+    async* keys() {
+        for await (const key of this.#db.keys()) {
+            yield key;
+        }
     }
 
-    values() {
-        return this.#db.values();
+    async* values() {
+        for await (const value of this.#db.values()) {
+            yield deserialize(value);
+        }
     }
 }
 
